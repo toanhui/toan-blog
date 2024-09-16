5474f6999d37110f2b8106d2efe2ac669da9e79e
Your Name <you@example.com>
2024-09-17 00:26:42 +0700

    add database

--- src/mysql.js
+++ src/mysql.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "be",
+    host: "ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "kdbe52grueryyy8x",
+    password: "p6lwqxh3to8dmnu8",
+    database: "yv7fzugo0czdm9nf",
 });
 
 module.exports = con;
